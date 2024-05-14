7af03dcd78ba6e15f81c18f70365c05b33caf0c5
ValCas18 <148790931+ValCas18@users.noreply.github.com>
2024-05-14 18:27:57 +0200

    Card first Iteration

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,30 +1,35 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import SearchBar from "./SearchBar";
+import { useSelector } from "react-redux";
 
 const WeatherCard = () => {
+	const city = useSelector((state) => state.city);
 	return (
 		<>
 			<SearchBar />
 			<Container className="mt-5">
 				<div className="weatherCard">
 					<Row className="">
-						<Col xs={12} md={6}>
-							<span className="fs-1">Rome</span>
+						<Col xs={12} md={6} className="d-flex justify-content-center align-items-center">
+							<span className="fs-1">{city.weather.name}</span>
 						</Col>
 						<Col xs={12} md={6} className="d-flex align-items-center justify-content-center">
-							<span>icona</span>
+							<img
+								src={`http://openweathermap.org/img/wn/${city.weather.weather[0].icon}@2x.png`}
+								alt="weather icon"
+							></img>
 						</Col>
 					</Row>
 					<Row className="cardSub">
-						<Col xs={12} md={4}>
-							<span>min-max</span>
+						<Col xs={12} md={4} className="d-flex justify-content-center align-items-center">
+							<span>Temp: {city.weather.main.temp}°C</span>
 						</Col>
-						<Col xs={12} md={4}>
-							<span>umidità</span>
+						<Col xs={12} md={4} className="d-flex flex-column justify-content-center">
+							<span>Min: {city.weather.main.temp_min}°C</span> <span>Max: {city.weather.main.temp_max}°C</span>
 						</Col>
-						<Col xs={12} md={4}>
-							<span>tempo</span>
+						<Col xs={12} md={4} className="d-flex justify-content-center align-items-center">
+							<span>Humidity: {city.weather.main.humidity}%</span>
 						</Col>
 					</Row>
 				</div>
